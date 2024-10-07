3f2241cd96ce4edd5c23868cfd4ceadac4ca02f3
dev sikarwar <de706097@gmail.com>
2024-10-07 22:24:29 +0530

    complete the project

--- logic.js
+++ logic.js
@@ -1,2 +1,9 @@
-let output = document.getElementById("output");
-output.contentDocument.body.innerHTML = "<h1> hello </h1>"
+function run(){
+    let htmlcode = document.getElementById("htmlcode").value;
+    let csscode = document.getElementById("csscode").value;
+    let jscode = document.getElementById("jscode").value;
+    let output = document.getElementById("show");
+    output.contentDocument.body.innerHTML = htmlcode;
+    output.contentDocument.head.innerHTML = "<style>" + csscode + "</style>";
+    output.contentWindow.eval(jscode);
+    }
